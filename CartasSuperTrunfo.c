b13ba441ecef24b7b05ce3978608d150f2e8033c
alyssonjrr <alyssonjrr7@gmail.com>
2025-04-08 16:10:40 +0000

    finalização ST

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-    // Definição das variáveis para cada atributo da cidade
+    // Definição das variáveis para cada atributo da cidade da carta 1
 
 int main(){
     char estado[50];
@@ -26,7 +26,7 @@ int main(){
     printf("População: ");
     scanf("%d", &populacao);
 
-    printf("Área (em km²): ");
+    printf("Área (km²): ");
     scanf("%f", &area);
 
     printf("PIB: ");
@@ -35,19 +35,69 @@ int main(){
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &numero_de_pontos_turisticos);
 
-    // Exibição da carta
+    // Exibição da carta 1
 
-    printf("----------------------------\n");
-    printf("CARTA 1"\n);
+    printf("----------------------------------\n");
+    printf("CARTA 1\n");
 
     printf("Estado: %s\n", estado);
     printf("Código: %s\n", codigo_da_cidade);
     printf("Nome da Cidade: %s\n", nome_da_cidade);
     printf("População: %d\n", populacao);
-    printf("Área (em km²): %f\n", area);
-    printf("PIB: %f\n", pib);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
     printf("Número de Pontos Turísticos: %d\n", numero_de_pontos_turisticos);
 
+    printf("----------------------------------\n");
+
+    // Definição das variáveis para cada atributo da cidade da carta 2
+
+    char estado2[50];
+    char codigo_da_cidade2[50];
+    char nome_da_cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int numero_de_pontos_turisticos2;
+
+    // Imputação das informações da carta 2 por parte do usuário
+
+    printf("Carta 2: \n");
+    printf("Estado: ");
+    scanf("%s", estado2);
+
+    printf("Código: ");
+    scanf("%s", codigo_da_cidade2);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", nome_da_cidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área (km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &numero_de_pontos_turisticos2);
+
+    // Exibição da carta 2
+
+    printf("----------------------------------\n");
+    printf("CARTA 2\n");
+
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo_da_cidade2);
+    printf("Nome da Cidade: %s\n", nome_da_cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", numero_de_pontos_turisticos2);
+
     return 0;
+
 
 }
